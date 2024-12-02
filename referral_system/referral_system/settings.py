264e74e0a2deed18d9ff06b29d5b1e509ca806fb
Илья Поташев <potaschev.ilya@yandex.ru>
2024-12-02 15:36:04 +0300

    fixed settings.py

--- referral_system/referral_system/settings.py
+++ referral_system/referral_system/settings.py
@@ -1,15 +1,18 @@
+import os
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-%g9hpuj-ba!*mnamgk07f-rbliy(7j(55i0-bht@4z3&*y#7-m'
+SECRET_KEY = os.getenv('SECRET_KEY', default='secret_key')
 
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG_MODE', default='False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='hosts').split(', ')
 
 
 INSTALLED_APPS = [
@@ -54,13 +57,28 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'referral_system.wsgi.application'
 
+USE_SQLITE = os.getenv('USE_SQLITE', default='True') == 'True'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if USE_SQLITE:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', ''),
+            'PORT': os.getenv('DB_PORT', 5432)
+        }
+    }
 
 AUTH_USER_MODEL = 'referall_app.ReferallUser'
 
